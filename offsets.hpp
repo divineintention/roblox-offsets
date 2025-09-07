02691c4ad2d79fdb758e66319e0fce38916e2b0d
lc <timmyxx55@gmail.com>
2025-09-07 18:33:26 -0400

    Update offsets.hpp

--- offsets.hpp
+++ offsets.hpp
@@ -1,1 +1,41 @@
 
+namespace offsets {
+    inline constexpr uintptr_t VisualEnginePointer = 0x6DEC878;
+    inline constexpr uintptr_t DataModelDeleterPointer = 0x7097010;
+    inline constexpr uintptr_t TaskSchedulerPointer = 0x7167538;
+    inline constexpr uintptr_t FakeDataModelPointer = 0x7097018;
+    inline constexpr uintptr_t FakeDataModelToDataModel = 0x1C0;
+    inline constexpr uintptr_t VisualEngine = 0x10;
+    inline constexpr uintptr_t Name = 0x88;
+    inline constexpr uintptr_t PlaceId = 0x1a0;
+    inline constexpr uintptr_t GameId = 0x198;
+    inline constexpr uintptr_t Workspace = 0x180;
+    inline constexpr uintptr_t Parent = 0x58;
+    inline constexpr uintptr_t ClassDescriptor = 0x18;
+    inline constexpr uintptr_t ClassDescriptorToClassName = 0x8;
+    inline constexpr uintptr_t Children = 0x68;
+    inline constexpr uintptr_t LocalPlayer = 0x128;
+    inline constexpr uintptr_t ModelInstance = 0x338;
+    inline constexpr uintptr_t Value = 0xd8;
+    inline constexpr uintptr_t Camera = 0x450;
+    inline constexpr uintptr_t CameraPos = 0x124;
+    inline constexpr uintptr_t CameraRotation = 0x100;
+    inline constexpr uintptr_t CameraSubject = 0xf0;
+    inline constexpr uintptr_t Gravity = 0x998;
+    inline constexpr uintptr_t UserId = 0x270;
+    inline constexpr uintptr_t FOV = 0x168;
+    inline constexpr uintptr_t Team = 0x258;
+    inline constexpr uintptr_t TeamColor = 0xd8;
+    inline constexpr uintptr_t CreatorId = 0x190;
+    inline constexpr uintptr_t Health = 0x19c;
+    inline constexpr uintptr_t MaxHealth = 0x1bc;
+    inline constexpr uintptr_t HipHeight = 0x1a8;
+    inline constexpr uintptr_t HumanoidStateId = 0x20;
+    inline constexpr uintptr_t JumpPower = 0x1b8;
+    inline constexpr uintptr_t WalkSpeed = 0x1dc;
+    inline constexpr uintptr_t WalkSpeedCheck = 0x3b8;
+    inline constexpr uintptr_t Primitive = 0x178;
+    inline constexpr uintptr_t Position = 0x154;
+    inline constexpr uintptr_t JobId = 0x140;
+    inline constexpr uintptr_t MoveDirection = 0x160;
+}
